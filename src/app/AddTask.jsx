253e445d2534ac068d18e21f4c4ecf2723218b2d
rofikul-resan <resan6231@gmail.com>
2023-06-14 21:09:30 +0600

    complite desing

--- src/app/AddTask.jsx
+++ src/app/AddTask.jsx
@@ -1,21 +1,96 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const AddTask = () => {
-  const { register, handleSubmit, reset } = useForm();
+  const [targetTime, setTargetTime] = useState(0);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const currentTime = new Date().getTime();
+  const timeValidate = (value) => {
+    if (value < 0) {
+      return "Only positive value can valid";
+    }
+    return;
+  };
+
+  const handleAddTask = (data) => {
+    const minute = +data?.minute || 0;
+    const hour = +data?.hour * 60 || 0;
+    const time = (minute + hour) * 60000;
+    if (time) {
+      setTargetTime(time + currentTime);
+    }
+    const taskData = {
+      title: data?.title,
+      targetTime: targetTime,
+      description: data?.description,
+    };
+    console.log(taskData);
+  };
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-center">Add Your new Task</h1>
-      <form>
-        <div className="form-control w-full max-w-xs">
+    <div className="px-6">
+      <h1 className="text-3xl font-bold text-center my-6">Add Your new Task</h1>
+      <form
+        onSubmit={handleSubmit(handleAddTask)}
+        className="bg-base-300 rounded-2xl md:w-7/12 mx-auto shadow-xl space-y-6 shadow-black/60 p-6"
+      >
+        <div className="form-control w-full ">
           <label className="label">
-            <span className="text-xl font-semibold italic">Title</span>
+            <span className="text-xl font-semibold italic">Task Title</span>
           </label>
           <input
-            {...register()}
+            {...register("title", { required: true })}
             type="text"
             placeholder="Type here"
-            className="input input-bordered w-full max-w-xs"
+            className="input input-bordered w-full"
           />
+        </div>
+        <div className="grid grid-cols-3 justify-center items-center">
+          <h1 className="text-xl  font-semibold items-center  ">
+            Target Time :{" "}
+          </h1>
+          <div className="form-control mx-2 ">
+            <input
+              {...register("hour", { validate: timeValidate })}
+              type="number"
+              placeholder="hour"
+              className="input input-bordered w-full"
+            />
+          </div>
+          <div className="form-control  ">
+            <input
+              {...register("minute", { validate: timeValidate })}
+              type="number"
+              placeholder="minute"
+              className="input input-bordered w-full"
+            />
+          </div>
+        </div>
+        {errors.hour && (
+          <p className="text-red-700 font-semibold ml-2">
+            {errors.hour.message}
+          </p>
+        )}
+        {errors.minute && (
+          <p className="text-red-700 font-semibold ml-2">
+            {errors.minute.message}
+          </p>
+        )}
+        <div>
+          <textarea
+            {...register("description", { required: true })}
+            className="input input-bordered w-full h-36 resize-none p-6"
+            placeholder="Description"
+          ></textarea>
+        </div>
+        <div>
+          <button type="submit" className="btn btn-primary btn-block ">
+            Add task
+          </button>
         </div>
       </form>
     </div>
