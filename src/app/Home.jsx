3a4eb7793079b7c1c9ab39439a7de2ffb4ea72f7
rofikul-resan <resan6231@gmail.com>
2023-06-14 15:01:22 +0600

    file setup

---
+++ src/app/Home.jsx
@@ -0,0 +1,32 @@
+const Home = () => {
+  return (
+    <div>
+      <div className="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col items-center justify-center">
+          {/* Page content here */}
+          <label
+            htmlFor="my-drawer-2"
+            className="btn btn-primary drawer-button lg:hidden"
+          >
+            Open navbar
+          </label>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+            {/* Sidebar content here */}
+            <li>
+              <a>All task</a>
+            </li>
+            <li>
+              <a>Add task</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
